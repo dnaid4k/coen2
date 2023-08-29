2a01f89cbfa6eb7d62da36ea291bed96593c09c5
Norbert.Duchnowski <dnaid4k@gmail.com>
2023-08-29 18:29:08 +0200

    rendering cell types 'code' | 'text'

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -75,7 +75,7 @@ const reducer = produce((
         default:
           return state;   
     }
-});
+}, initialState);
 const randomId = () => {
   return Math.random().toString(36).substring(2, 5);
 };
