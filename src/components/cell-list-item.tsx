2b252a02bfbe6f98de076127a09f741c29780b6f
Norbert.Duchnowski <dnaid4k@gmail.com>
2023-08-30 16:08:42 +0200

    fontawesome library plus css for buttons

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,3 +1,4 @@
+import './cell-list-item.css';
 import { Cell } from "../state";
 import CodeCell from "./code-cell";
 import TextEditor from "./text-editor";
@@ -10,14 +11,23 @@ interface CellListItemProps {
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
     let child: JSX.Element;
     if (cell.type === 'code') {
-        child = <CodeCell cell={cell} />
-    } else {
-        child = <TextEditor cell={cell}/>
-    }
-    return <div>
+        child = <>
+        <div className="action-bar-wrapper">
         <ActionBar id={cell.id}/>
+        </div>
+        <CodeCell cell={cell} />
+        </>
+    } else {
+        child = <>
+        <TextEditor cell={cell}/>
+        <ActionBar id={cell.id}/>
+        </>
+    }
+    return(
+    <div className="cell-list-item">
         {child}
-        </div>;
+        </div>
+    );
 };
 
 export default CellListItem;
